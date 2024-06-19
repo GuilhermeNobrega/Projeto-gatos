81fb54909ad4e5ac6b61e7c027ca13a8d27fe0c9
Hector Pérez <hector.perez@oraex.onmicrosoft.com>
2024-06-19 14:14:40 -0300

    adicionando alterações

--- Back_end/main.py
+++ Back_end/main.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-Configurações do MySQL
+
 app.config['MYSQL_HOST'] = '10.88.80.3'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '|:rrNI%iLJ\F-ip3'
